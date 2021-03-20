954696c7e3f3ea8bcc52d1d44eee485ecb9c18cf
rikicop <ruperto1@protonmail.com>
2021-03-20 18:13:47 -0400

    urls modif

--- djimages_project/urls.py
+++ djimages_project/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static 
 
-from inmuebles import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inmueble_view, name='blog'),
-    path('<int:id>/', views.detail_view, name='detail')
+    path('',include('inmuebles.urls')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
